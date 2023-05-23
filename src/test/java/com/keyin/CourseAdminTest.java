46a1eaf2448399332382ff30c8a0348d005730e0
DevinAugot <105358840+DevinAugot@users.noreply.github.com>
2023-05-23 09:12:32 -0230

    Initial commit

---
+++ src/test/java/com/keyin/CourseAdminTest.java
@@ -0,0 +1,33 @@
+package com.keyin;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CourseAdminTest {
+
+    @Test
+    public void testGetCourseListByStudent() {
+        CourseAdmin courseAdmin = new CourseAdmin();
+
+        Student student = new Student();
+        student.setId(1);
+        student.setName("Devin");
+
+        Course course = new Course();
+        course.setId(1);
+        course.setName("Java");
+
+        courseAdmin.addCourseToStudent(student, course);
+
+        Student studentToFind = new Student();
+        studentToFind.setId(1);
+        studentToFind.setName("Devin");
+
+        Course courseToFind = new Course();
+        courseToFind.setId(1);
+        courseToFind.setName("Java");
+
+        Assertions.assertTrue(courseAdmin.getCourseListByStudent(studentToFind).contains(courseToFind));
+
+    }
+}
